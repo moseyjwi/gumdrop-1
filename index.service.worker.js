ffa9e6b03234044cf9ac1549cc8de103d13212f2
moseyjwi <moseyjwi@outlook.com>
2026-06-20 17:46:59 -0400

    oops

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781991033|25793890383';
+const CACHE_VERSION = '1781992014|26775201991';
 /** @type {string} */
 const CACHE_PREFIX = 'Gumdrop-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
